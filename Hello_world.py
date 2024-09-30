36a32d9f4cdb6792b980d14b34cb990f0342be05
Rohail Sheikh (s5422325) <s5422325@bournemouth.ac.uk>
2024-09-30 16:25:44 +0100

    initial message via vscode

---
+++ Hello_world.py
@@ -0,0 +1,21 @@
+name = "rohail"
+age = 21
+print(f"{name} is {age} years old")
+print("but {} is {} years old".format("another person", 22))
+
+a = "this sentence has been "
+b = "concatonated"
+print(a + b)
+
+print(1 + 2) # addition
+
+print(3 % 2) # divide and give out remainder
+
+print(9 // 3) # divide, int result
+print(8 // 3) # divide, int result
+
+print(8 / 3) # normal divide
+
+print(4 * 2) # multiply
+
+print(4 ** 3) # to the power of
