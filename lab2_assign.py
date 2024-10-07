a4a8a11470f9172e51f196ee28a1cabd69943575
antendis <antendis@gmail.com>
2024-10-07 16:11:11 +0100

    message

--- lab2_assign.py
+++ lab2_assign.py
@@ -1,6 +1,29 @@
-
-
 a = 11
 a += 5
 a_type = type(a)
 print(a, " is a the type ", a_type)
+
+b = 16
+b //= 2
+print(b)
+
+
+tom_marvolo_riddle = "Tom Marvolo Riddle"
+
+print(tom_marvolo_riddle[:3])
+print(tom_marvolo_riddle[4:-6])
+print(tom_marvolo_riddle[-6:])
+
+
+
+
+
+
+
+
+voldemort = "I am lord voldemort"
+print(voldemort)
+
+
+
+ 
