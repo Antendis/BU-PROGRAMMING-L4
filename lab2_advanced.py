e71ac7843ba38ab64041dd9354d213f97e736696
antendis <antendis@gmail.com>
2024-10-07 16:55:32 +0100

    trying a thing

--- lab2_advanced.py
+++ lab2_advanced.py
@@ -1,1 +1,8 @@
+# takes number
+# prints that many spaces -1 then 1 1
+# repeat on further lines with one less space and one more number of a higher number
+# until reaches given number
+
+
+
 number = input("Give me a number: ")
