730d3bc2aede61f74cbb06a7ae3e272c693f0ee3
antendis <antendis@gmail.com>
2024-10-07 16:38:15 +0100

    some more learning

--- lab2_assign.py
+++ lab2_assign.py
@@ -7,12 +7,20 @@ b = 16
 b /= 2 # use // for int
 print(b, " is type ", type(b))
 
-tom_marvolo_riddle = "Tom Marvolo Riddle"
+c = 2.5 # is a float
+print(int(c)) # can be changed to int
 
-print(tom_marvolo_riddle[:3])
-print(tom_marvolo_riddle[4:-6])
-print(tom_marvolo_riddle[-6:])
+tom_marvolo_riddle = "Tom Riddle"
+
+print(tom_marvolo_riddle[:3].upper()) # the [] chooses which part of the string, the .upper makes it all upprercase
+print(tom_marvolo_riddle[-6:].lower()) 
 
 # tom_marvolo_riddle[0] = "I"
-#test above didntwork because strings cant be modified?
+# test above didnt work because strings are immutable?
+
+print(tom_marvolo_riddle.replace("Tom", "Vol"))
+print(tom_marvolo_riddle.replace("Tom Ri", "Voldem "))
+print(tom_marvolo_riddle.replace("Tom Riddle", "Lord Voldemort"))
+
+
 
